5e498fdbe00d9b5efe6b3e486ad65d43d45f035b
Nilesh-dvij <nilesh.dvij@gmail.com>
2023-01-10 15:45:26 +0530

    initial commit

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "Your Api Key",
+  key: "YOUR API KEY",
   base: "https://api.openweathermap.org/data/2.5/",
 };
