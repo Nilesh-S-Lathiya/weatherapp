11722441e6107e1b333712aa9fa1c90fa8d4ce04
Nilesh-dvij <nilesh.dvij@gmail.com>
2023-01-30 15:58:25 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ function App() {
       <div className="footer-info">
         <a href="#github">
           Download Source Code
-        </a>{" "}
-        | Developed by{" "}
+        </a>
+        | Developed by
         <a target="_blank" href="#">
           Nilesh
-        </a>{" "}
-        | Powered by{" "}
+        </a>
+        | Powered by
         <a target="_blank" href="">
           ReactJS
         </a>
